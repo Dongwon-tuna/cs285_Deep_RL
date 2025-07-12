5e0b81b762979c153c9aac5be1964648d37ea6e9
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2025-07-12 16:23:59 +0900

    Update test.py

--- myProjects/cartpole_stochastic_policy/test.py
+++ myProjects/cartpole_stochastic_policy/test.py
@@ -29,22 +29,39 @@ class MLPPolicy(nn.Module):
 env = gym.make("CartPole-v1", render_mode="human")
 obs_dim = env.observation_space.shape[0]
 act_dim = env.action_space.n
+print("obs dim: ")
+print(obs_dim) # 관측의 차원임   4
+print("act dim: ") 
+print(act_dim)# 행동의 차원임    2
 
-policy = MLPPolicy(obs_dim, act_dim).to(device)
+policy = MLPPolicy(obs_dim, act_dim).to(device)  #GPU로 모델 옮기기
 
 obs, _ = env.reset()
 done = False
 total_reward = 0
 
-while not done:
+while True:
+
+    # CartPole 환경의 관측값(obs)은 다음과 같은 4개의 요소:
+    # obs[0] → cart position         : 카트의 수평 위치 (x 좌표)
+    # obs[1] → cart velocity         : 카트의 속도
+    # obs[2] → pole angle            : 막대의 각도 (θ), 수직에서 얼마나 기울었는지 (rad 단위)
+    # obs[3] → pole angular velocity : 막대의 각속도 (회전 속도)
+
+
     obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(device)
 
     with torch.no_grad():
         logits = policy(obs_tensor)
         action = torch.argmax(logits, dim=-1).item()
 
+    print(f"obseravtion: {obs}")  # ← 여기 추가
+
     obs, reward, done, _, _ = env.step(action)
     total_reward += reward
+
+
+
 
 env.close()
 print("Total Reward:", total_reward)
